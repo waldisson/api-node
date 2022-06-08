a8cf75f53d89ec1f5151345691b07c9289cc057c
Waldisson Bittencourt <waldisson@hotmail.com>
2022-06-08 19:27:01 -0300

    adjust

--- server.js
+++ server.js
@@ -5,12 +5,3 @@ const PORT = 3000;
 
 app.use('/api', require('./src/routes'))
 app.listen(PORT, () => console.log(`Listtening on ${PORT}`));
-
-// const http = require('http');
-
-// const requestListener = function(req, res) {
-//   res.writeHead(200);
-//   res.end('Hello, World!');
-// }
-// const server = http.createServer(requestListener);
-// server.listen(PORT);
